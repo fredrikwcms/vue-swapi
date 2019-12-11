10f1b7cd600bc46cc10489487c2748e16c2ef772
fredrik <fl@thehiveresistance.com>
2019-12-11 10:17:46 +0100

    Changed API to SWAPI :)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CoinInfo/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CoinInfo from './components/CoinInfo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    CoinInfo
   }
 }
 </script>
---
+++ src/components/CoinInfo.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="row">
+        <div v-for="character in characters.data.results" v-bind:key="character.name" class="col-3">
+            {{ character.name }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            characters: this.results
+        }
+    },
+    mounted() {
+    const axios = require('axios');
+        axios
+            .get('https://swapi.co/api/people/')
+            .then(response => (this.characters = response))
+    }
+}
+</script>
